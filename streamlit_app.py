8387a98a5bd5897cff3f5286bc18b72901fa3a73
sachinkjadhav <59939019+sachinkjadhav@users.noreply.github.com>
2022-08-15 21:26:03 +0530

    Update streamlit_app.py
    Resolved issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 
 import streamlit
-import pandas
+#import pandas
 import snowflake.connector
+import urllib.error import URLError
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')

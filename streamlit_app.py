3224300fb849a03b619640440077806bee55ee76
sachinkjadhav <59939019+sachinkjadhav@users.noreply.github.com>
2022-08-15 22:50:37 +0530

    Update streamlit_app.py
    Issue resolved

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,10 +43,10 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.text(my_data_row)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-        my_cnx.execute("insert into fruit_load_list (FRUIT_NAME) "
+        my_cur.execute("insert into fruit_load_list (FRUIT_NAME) "
         "VALUES(%s)", (
         new_fruit    
         ))
